fe3858bf0c15cac8616da09fec4bbd1f560df12f
raise <C0A24048ba@edu.teu.ac.jp>
2025-06-24 16:29:34 +0900

    追加機能２：複数ビーム

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -180,6 +180,7 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
     score=Score()  # スコアのインスタンスを生成
+    beams=[]# ビームのリストを初期化
     # bomb = Bomb((255, 0, 0), 10)
     # bombs=[]
     # for _ in range(NUM_OF_BOMBS):
@@ -192,9 +193,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-            #     # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:#     # スペースキー押下でBeamクラスのインスタンス生成
+                beams.append(Beam(bird))  # ビームを生成してリストに追加
+                # beam = Beam(bird)   #ビーム追加         
         screen.blit(bg_img, [0, 0])
         
         # if bomb is not None:
@@ -210,23 +211,30 @@ def main():
                 return
         
         # if beam is not None:
-        for i,bomb in enumerate(bombs):
-            if beam is not None:
-                if beam.rct.colliderect(bomb.rct):# ビームと爆弾が当たった場合
-                    beam=None
-                    bombs[i]=None
+        # for i,bomb in enumerate(bombs):
+        #     if beam is not None:
+        #         if beam.rct.colliderect(bomb.rct):# ビームと爆弾が当たった場合
+        #             beam=None
+        #             bombs[i]=None
+        #             bird.change_img(6, screen)
+        #             score.increment()#スコア加算
+        for beam in beams:
+            for i, bomb in enumerate(bombs):
+                if bomb is not None and beam is not None and beam.rct.colliderect(bomb.rct):
+                    bombs[i] = None
+                    beams[beams.index(beam)] = None
                     bird.change_img(6, screen)
-                    score.increment()#スコア加算
+                    score.increment() 
+
 
         bombs = [bomb for bomb in bombs if bomb is not None]  # 爆弾リストからNoneを除去
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        if beam is not None:
-            # ビームが存在する場合のみ更新
-            beam.update(screen)
-        for bomb in bombs:
-            # 爆弾が存在する場合のみ更新   
+        for beam in beams:
+            if beam is not None:# ビームが存在する場合のみ更新
+                beam.update(screen)
+        for bomb in bombs:# 爆弾が存在する場合のみ更新   
             bomb.update(screen)
         score.update(screen)# スコアを表示
         pg.display.update()
